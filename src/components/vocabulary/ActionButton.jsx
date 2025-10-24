97fb6b37007b86761cdeeb83fda2431936b4cd85
zanchuyn <vanchuyenle2004@gmail.com>
2025-10-24 21:09:19 +0700

    feat/pagination + mini bugs fixed

--- src/components/vocabulary/ActionButton.jsx
+++ src/components/vocabulary/ActionButton.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const ActionButton = ({ icon: Icon, label, bgColor, link }) => {
 
-  const navigate = useNavigate();
-
   return (
-    <button
-      onClick={() => navigate(link)}
-      className="d-flex align-items-center gap-1 rounded px-2 py-1 border-0"
-      style={{ backgroundColor: bgColor, cursor: 'pointer' }}
-    >
-      <Icon size={24} className="me-1 text-white" />
-      <span className="text-white">{label}</span>
-    </button>
+    <Link to={link}>
+      <button
+        className="d-flex align-items-center gap-1 rounded px-2 py-1 border-0"
+        style={{ backgroundColor: bgColor, cursor: 'pointer' }}
+      >
+        <Icon size={24} className="me-1 text-white" />
+        <span className="text-white">{label}</span>
+      </button>
+    </Link>
   )
 }
 
